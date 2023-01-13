0a75da0ed2a704d26fb3e6ecd664c1c0dc4244d9
Jeff Osundwa <mulamajeff@tuta.io>
2023-01-13 12:45:10 +0300

    Basic annotations - concat

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" function concat """
+
+
+def concat(str1: str, str2: str) -> str:
+    """concats two strings
+    returns: soncatenated string"""
+    return str1 + str2
