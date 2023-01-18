ab979aa6fb877df2dfc02bfd691497e877726aa0
Jeff Osundwa <mulamajeff@tuta.io>
2023-01-18 11:29:04 +0300

    complex types - functions

---
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Complex types - functions """
+
+from typing import Callable
+
+
+def make_multiplier(multiplier: float) -> Callable[[float], float]:
+    """creates a multiplier function"""
+    return lambda x: x * multiplier
